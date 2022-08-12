a8d6d3df507c826d7f76d1398fc94ce340aaf48a
Kevin Cui <kevincui113@gmail.com>
2022-08-12 10:18:49 -0400

    Added Dish Component and Dishes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Dish } from "./Dish";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <h1>HTN Restaurant</h1>
+      <Dish name="Pizza 🍕" price={10}/>
+      <Dish name="Pasta 🍝" price={20}/>
+      <Dish name="Soup 🍜" price={15}/>
     </div>
   );
 }
